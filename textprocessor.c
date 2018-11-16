2c5df4a83af88877b4b244a1a419d3e4d4200bcb
gasbo <gasullb@gmail.com>
2018-11-16 18:17:11 +0100

    improved textprocessor

--- textprocessor.c
+++ textprocessor.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "naive.h"
 
-int counter(struct NODE* node){
+long long counter(struct NODE* node){
 
-	int count = 0;
+	long long count = 0;
 	for (int i = 0; i < ALPHABET_WIDTH; ++i)
 	{
 		if (node->edges[i] != NULL ){
@@ -18,10 +18,9 @@ int counter(struct NODE* node){
 int main (){
 
 	char text[100000] = {0};
-	char p_text[10000] = {0};
+	char c;
 	int Q,W, start, i;
 
-	int result;
 	struct NODE* tree;
 
 	scanf("%s", text);
@@ -32,16 +31,18 @@ int main (){
 	{
 		scanf("%d", &start);
 
-		strcpy(p_text, text + start);
-		p_text[W] = 0;
+		c = *(text + start + W);
+		*(text + start + W) = 0;
 
-		tree = getSuffixTree(p_text);
+		tree = getSuffixTree(text + start);
 
-		result = counter(tree);
+		*(text + start + W) = c;
+
+		printf("%lld\n", counter(tree));
 
 		freeTree(tree);
 
-		printf("%d\n", result);
+		// printf("%.*s\n", W, text + start);
 		
 	}
 
